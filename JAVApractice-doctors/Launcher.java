87f750d1a9107ed7dabb48ab7ccf7626835bfbfe
mdy6099 <mdy6099@naver.com>
2019-03-26 01:23:02 +0900

    little change for test

--- JAVApractice-doctors/Launcher.java
+++ JAVApractice-doctors/Launcher.java
@@ -18,5 +18,17 @@ public class Launcher {
 		familyDoctorInstance.giveAdvice();
 		surgeonInstance.treatPatient();
 		surgeonInstance.makeIncision();	
+		// Doctor s1=new Surgeon(); //Understanding Object.equals() method
+		// Doctor f1=new FamilyDoctor();
+		// Doctor s2=new Surgeon();
+		// Surgeon s1Clone= (Surgeon)s1;
+		// Surgeon s2Clone= (Surgeon)s2;
+		// System.out.println(s1.getClass());
+		// System.out.println(f1.getClass());
+		// System.out.println("s1 is equal to f1?: "+s1.equals(f1));
+		// System.out.println("s1 is equal to s2?: "+s1.equals(s2));
+		// System.out.println("s1 is equal to s1?: "+s1.equals(s1));
+		// System.out.println("s1 is equal to s1Clone?: "+s1.equals(s1Clone));
+		// System.out.println("s1 is equal to s2Clone?: "+s1.equals(s2Clone));
 	}	
 }
