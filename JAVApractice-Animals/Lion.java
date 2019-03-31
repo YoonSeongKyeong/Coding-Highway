1ee179f4156042c76f4921e224affbe235dc06df
mdy6099 <mdy6099@naver.com>
2019-03-31 14:21:28 +0900

    lion

--- JAVApractice-Animals/Lion.java
+++ JAVApractice-Animals/Lion.java
@@ -6,7 +6,7 @@ public class Lion extends Feline {
     @Override 
     public void eat() {// we can check if meal is what this animal would eat
         hunger=0;
-        System.out.println("(Lion eat)GGRRGGG");
+        System.out.println("(Lion eat)GGRRGGGUMMMMMMM");
     }
     @Override
     public void sleep() {
