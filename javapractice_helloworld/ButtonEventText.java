e973942552c6b74061f9f1a1744146205ea98548
mdy6099 <mdy6099@naver.com>
2019-04-07 23:00:16 +0900

    ok

---
+++ javapractice_helloworld/ButtonEventText.java
@@ -0,0 +1,35 @@
+package javapractice_helloworld;
+
+import javax.swing.*;
+
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.event.*;
+
+public class ButtonEventText implements ActionListener{ 
+    private static String leftLabel = "Number of button clicks : ";
+    private int numClicks = 0;
+    JLabel label = new JLabel(leftLabel + "0  ");
+
+    public void go (String title) {
+        JFrame frame = new JFrame(title);
+        JButton button = new JButton("Swing Button!");
+        button.addActionListener(this);
+
+        JPanel panel = new JPanel();
+        panel.add(button);
+        panel.add(label);
+        frame.getContentPane().add(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+    public void actionPerformed(ActionEvent event) {
+        label.setText(leftLabel + ++numClicks);
+    }
+
+    public static void main(String[] args) {
+        ButtonEventText button = new ButtonEventText();
+        button.go("Event handling");
+    }
+}
